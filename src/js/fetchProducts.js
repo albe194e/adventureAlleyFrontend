b34d0305cd93907c905104e10608e8876590ba62
BrightBlueBird <99009695+BrightBlueBird@users.noreply.github.com>
2023-03-16 10:58:17 +0100

    Made table of products

--- src/js/fetchProducts.js
+++ src/js/fetchProducts.js
@@ -16,13 +16,24 @@ async function createProductTable() {
         const cell2 = row.insertCell(1)
         const cell3 = row.insertCell(2)
         const cell4 = row.insertCell(3)
+        const cell5 = row.insertCell(4)
         cell1.textContent = product.name
         cell2.textContent = product.ageLimit
-        cell3.textContent = product.type
-        cell4.textContent = product.price
+        cell3.textContent = product.price
+        cell4.textContent = product.equipmentid
+        console.log(product)
         table.appendChild(cell1)
         table.appendChild(cell2)
         table.appendChild(cell3)
         table.appendChild(cell4)
+
+        const deletebtn = document.createElement("button")
+        deletebtn.textContent = "delete"
+        deletebtn.addEventListener("click", () => {
+            row.remove()
+        })
+        table.appendChild(cell5)
     })
+
 }
+createProductTable()
