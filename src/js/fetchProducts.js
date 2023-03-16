b9e8662db30cfe88174daeade143b6ac5d287e91
BrightBlueBird <99009695+BrightBlueBird@users.noreply.github.com>
2023-03-16 13:25:40 +0100

    Made table of products

--- src/js/fetchProducts.js
+++ src/js/fetchProducts.js
@@ -1,39 +1,32 @@
+
+
 console.log("fetchProducts")
 const urlProducts = "http://localhost:8081/products"
 const table = document.getElementById("productTable")
+
 function fetchAny(url) {
     console.log(url)
     return fetch(url).then((response) => response.json())
 }
 
 
+
 async function createProductTable() {
     const products = await fetchAny(urlProducts)
     console.log(products)
-    products.forEach(product => {
-        const row = table.insertRow()
-        const cell1 = row.insertCell(0)
-        const cell2 = row.insertCell(1)
-        const cell3 = row.insertCell(2)
-        const cell4 = row.insertCell(3)
-        const cell5 = row.insertCell(4)
-        cell1.textContent = product.name
-        cell2.textContent = product.ageLimit
-        cell3.textContent = product.price
-        cell4.textContent = product.equipmentid
-        console.log(product)
-        table.appendChild(cell1)
-        table.appendChild(cell2)
-        table.appendChild(cell3)
-        table.appendChild(cell4)
+    const tableRowsData = products.map(product => `
+    <tr>
+      <td>${product.name}</td>
+      <td>${product.ageLimit}</td>
+      <td>${product.price}</td>
+      <td>${product.equipment.name}</td>
+    </tr>`)
 
-        const deletebtn = document.createElement("button")
-        deletebtn.textContent = "delete"
-        deletebtn.addEventListener("click", () => {
-            row.remove()
-        })
-        table.appendChild(cell5)
-    })
+  let tableAsString = tableRowsData.join("\n")
+    console.log(tableAsString)
+    document.getElementById("productTable").innerHTML = tableAsString
+
 
 }
+
 createProductTable()
