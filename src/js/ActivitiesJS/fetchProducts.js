a5da55ecfbc052ba21067fcfa65622281cdc083f
BrightBlueBird <99009695+BrightBlueBird@users.noreply.github.com>
2023-03-17 14:25:30 +0100

    Made table of products

--- src/js/ActivitiesJS/fetchProducts.js
+++ src/js/ActivitiesJS/fetchProducts.js
@@ -1,25 +1,42 @@
 
 
 console.log("fetchProducts")
-const urlProducts = "http://localhost:8081/products"
+const deleteURL = "http://localhost:8080/deleteActivity"
+const urlProducts = "http://localhost:8080/products"
 const table = document.getElementById("productTable")
+
+
+
 
 function fetchAny(url) {
     console.log(url)
     return fetch(url).then((response) => response.json())
 }
 
+async function deleteActivity(id) {
+    console.log(id)
+
+    const deleteReq = {
+        method: "DELETE"
+    }
+    await fetch(deleteURL + "/" + id, deleteReq).catch((error) => console.log(error));
+    createProductTable()
+
+}
+
 
 
 async function createProductTable() {
+
     const products = await fetchAny(urlProducts)
-    console.log(products)
+
     const tableRowsData = products.map(product => `
     <tr>
       <td>${product.name}</td>
       <td>${product.ageLimit}</td>
       <td>${product.price}</td>
       <td>${product.equipment.name}</td>
+      <td><button id="${product.activityId}" onclick="deleteActivity(${product.activityId})">delete</button></td>
     </tr>`)
 
   let tableAsString = tableRowsData.join("\n")
@@ -28,5 +45,6 @@ async function createProductTable() {
 
 
 }
+
 
 createProductTable()
